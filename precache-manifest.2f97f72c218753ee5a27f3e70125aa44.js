1ddfb1d7e479883ef111d7837c016ccce5f6a51a
Artem Rakitskiy <ar.rakitskiy@gmail.com>
2020-10-24 10:04:41 +0300

    Updates

---
+++ precache-manifest.2f97f72c218753ee5a27f3e70125aa44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c532ef89c09df89abb9103b542334ec",
+    "url": "/%3Creponame%3E/index.html"
+  },
+  {
+    "revision": "dfd90319df784f92f47b",
+    "url": "/%3Creponame%3E/static/css/main.a6e9a7b8.chunk.css"
+  },
+  {
+    "revision": "30118ffbd3fb81ebec88",
+    "url": "/%3Creponame%3E/static/js/2.a80242ce.chunk.js"
+  },
+  {
+    "revision": "2619bd7cd28ada2543c6f4df5fc1d585",
+    "url": "/%3Creponame%3E/static/js/2.a80242ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd90319df784f92f47b",
+    "url": "/%3Creponame%3E/static/js/main.9e1ce05e.chunk.js"
+  },
+  {
+    "revision": "1aee7d3c6c2c194168b5",
+    "url": "/%3Creponame%3E/static/js/runtime-main.10802612.js"
+  }
+]);
